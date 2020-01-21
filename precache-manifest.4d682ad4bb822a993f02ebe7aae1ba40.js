237131faecaba11d30387a6391b2718283a1491c
dwax1324 <dwax1324@gmail.com>
2020-01-21 19:18:07 +0900

    Updates

---
+++ precache-manifest.4d682ad4bb822a993f02ebe7aae1ba40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fb08aab2d32d3d20e13f4c7b70721b",
+    "url": "/woojong-movie/index.html"
+  },
+  {
+    "revision": "5c5c8fb94f75178d26d3",
+    "url": "/woojong-movie/static/css/main.ead16241.chunk.css"
+  },
+  {
+    "revision": "b8d892a451523bc94754",
+    "url": "/woojong-movie/static/js/2.c5a43ff7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/woojong-movie/static/js/2.c5a43ff7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c5c8fb94f75178d26d3",
+    "url": "/woojong-movie/static/js/main.eca7328a.chunk.js"
+  },
+  {
+    "revision": "1927659dfa672461afa3",
+    "url": "/woojong-movie/static/js/runtime-main.5ad74f16.js"
+  }
+]);
